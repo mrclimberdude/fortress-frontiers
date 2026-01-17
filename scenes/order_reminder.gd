9cd2afe8a2b47e518949f7e45c6e3dec1f61200f
mrclimberdude <matt.bisson98@gmail.com>
2026-01-17 16:32:19 -0500

    Added a different shade for just purchased units instead of the needs orders color

--- scenes/order_reminder.gd
+++ scenes/order_reminder.gd
@@ -1,5 +1,9 @@
 extends TileMapLayer
 
+const ORDER_REMINDER_SOURCE_ID: int = 2
+const ORDER_REMINDER_ALT_ID: int = 1
+const ORDER_REMINDER_TILE: Vector2i = Vector2i(1, 1)
+const ORDER_REMINDER_NEW_TILE: Vector2i = Vector2i(0, 2)
 
 func _ready() -> void:
 	pass
@@ -9,4 +13,5 @@ func highlight_unordered_units(player_id):
 	var units = $"..".get_all_units()[player_id]
 	for unit in units:
 		if not unit.is_base and not unit.is_tower and not unit.ordered:
-			set_cell(unit.grid_pos, 2, Vector2i(1,1), 1)
+			var coords = ORDER_REMINDER_NEW_TILE if unit.just_purchased else ORDER_REMINDER_TILE
+			set_cell(unit.grid_pos, ORDER_REMINDER_SOURCE_ID, coords, ORDER_REMINDER_ALT_ID)
